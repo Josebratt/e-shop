a9895451e5dda84c17d76887d84f828fa9ed8213
Jose <josebratt@gmail.com>
2022-11-30 16:47:36 -0400

    create navbar, sidebar and footer component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AdminComponent } from './admin/admin.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
