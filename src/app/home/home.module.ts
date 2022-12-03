6780c05bfbcb687bf2aafa9e54df448287d677c4
Jose <josebratt@gmail.com>
2022-12-03 15:24:27 -0400

    create features component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,15 @@ import { HomeRoutingModule } from './home.routes';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { FeaturesComponent } from './pages/features/features.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FeaturesComponent
   ],
   imports: [
     CommonModule,
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './pages/features/features.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-    // children: [
-    //   { path: 'category', component: ListCategoryComponent },
-    // ]
+    component: HomeComponent,
+    children: [
+      { path: '', component: FeaturesComponent },
+    ]
   },
 ];
 
