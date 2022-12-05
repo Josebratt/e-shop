9f879ff28655c78995e1a4c64773ee7b5a37a096
Jose <josebratt@gmail.com>
2022-12-05 16:30:51 -0400

    implement getcategories method

--- src/app/admin/pages/categories/list/list.component.ts
+++ src/app/admin/pages/categories/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { Category } from 'src/app/interfaces/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-list',
@@ -9,9 +11,25 @@ import { Category } from 'src/app/interfaces/category';
 export class ListComponent implements OnInit {
   categories: Category[] = [];
 
-  constructor() { }
+  endsubs$: Subject<unknown> = new Subject();
+
+  constructor(
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this._getCategories();
+  }
+
+  ngOnDestroy(): void {
+    this.endsubs$.next(true);
+    this.endsubs$.complete();
+  }
+
+  private _getCategories() {
+    this.categoryService.getCategories().pipe(takeUntil(this.endsubs$)).subscribe(
+      (data) => { this.categories = data }
+    )
   }
 
   onUpdate(id: string) {
