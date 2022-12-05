0ce393a044dfd3298a40e25a84a527b30eda3f71
Jose <josebratt@gmail.com>
2022-12-05 16:22:05 -0400

    update category list component

--- src/app/admin/pages/categories/list/list.component.ts
+++ src/app/admin/pages/categories/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/interfaces/category';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  categories: Category[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onUpdate(id: string) {
+
+  }
+
+  onDelete(id: string) {
+    
   }
 
 }
