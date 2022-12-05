9f15f4d09cd963c9f9c6b7c51f6106352b989174
Jose <josebratt@gmail.com>
2022-12-05 16:28:51 -0400

    create getcategories

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Category } from '../interfaces/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  API_URL = environment.apiURL + 'categories';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todas las categorias
+   */
+   getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.API_URL);
+  }
 }
