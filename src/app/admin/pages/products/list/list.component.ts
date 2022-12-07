aa2667fc4df1e3c5fbc1304cce9eec2e36545bdc
Jose <josebratt@gmail.com>
2022-12-06 20:32:50 -0400

    products list update

--- src/app/admin/pages/products/list/list.component.ts
+++ src/app/admin/pages/products/list/list.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../../../services/product.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListProductComponent implements OnInit {
+export class ListProductComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
+  endsubs$: Subject<unknown> = new Subject();
 
-  constructor() { }
+  constructor(
+    private productService: ProductService
+    ) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  ngOnDestroy(): void {
+    this.endsubs$.next(true);
+    this.endsubs$.complete();
+  }
+
+  getProducts() {
+    this.productService.getProducts().pipe(takeUntil(this.endsubs$)).subscribe({
+      next: (data) => {
+        this.products = data;
+        setTimeout(() => {
+          $('#table').DataTable({
+            retrieve: true,
+            pagingType: 'full_numbers',
+            pageLength: 5,
+            processing: true,
+            lengthMenu: [5, 10, 25],
+            dom: 'Bfrtip'              
+          });
+        }, 1);
+      },
+      error: (error) => console.error(error),
+    })
   }
 
   onShow(id: string) {
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,62 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Product } from '../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  API_URL = environment.apiURL + 'products';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  /**
+   * get all products
+   * @returns 
+   */
+  getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.API_URL);
+  }
+
+  /**
+   * get product by id
+   * @param id
+   * @returns 
+   */
+  getProduct(id: string): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.API_URL}/${id}`);
+  }
+
+  /**
+   * create a new product
+   * @param product
+   * @returns 
+   */
+  saveProduct(product: FormData): Observable<Product> {
+    return this.httpClient.post<Product>(this.API_URL, product);
+  }
+
+  /**
+   * update a product
+   * @param id 
+   * @param productData 
+   * @returns 
+   */
+  updateProduct(id: string, productData: FormData): Observable<Product> {
+    return this.httpClient.put<Product>(`this.API_URL/${id}`, productData);
+  }
+
+  /**
+   * delete a product
+   * @param id 
+   * @returns 
+   */
+  deleteProduct(id: string): Observable<Product> {
+    return this.httpClient.delete<Product>(`${this.API_URL}/${id}`);
+  }
 }
