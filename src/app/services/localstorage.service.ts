43039631beeb18864a042aff3c3ab15f5923b0f4
Jose <josebratt@gmail.com>
2022-12-18 14:18:11 -0400

    document local storage Service

--- src/app/services/localstorage.service.ts
+++ src/app/services/localstorage.service.ts
@@ -9,12 +9,25 @@ export class LocalstorageService {
 
   constructor() { }
 
+  /**
+   * setToken 
+   * @param data 
+   */
   setToken(data: any) {
     localStorage.setItem(TOKEN, data);
   }
+
+  /**
+   * Get the current token
+   * @returns 
+   */
   getToken(): string | null {
     return localStorage.getItem(TOKEN);
   }
+
+  /**
+   * Delete the current token
+   */
   removeToken() {
     localStorage.removeItem(TOKEN);
   }
