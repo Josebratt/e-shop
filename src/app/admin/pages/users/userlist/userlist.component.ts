20189e4546dfc0163e6aef6ffb6b308ac8c10375
Jose <josebratt@gmail.com>
2022-12-18 20:31:52 -0400

    update userlist, userservice and adduser

--- src/app/admin/pages/users/userlist/userlist.component.ts
+++ src/app/admin/pages/users/userlist/userlist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-userlist',
@@ -9,14 +11,40 @@ import { User } from 'src/app/interfaces/user';
 export class UserlistComponent implements OnInit {
 
   users: User[] = [];
+  endsubs$: Subject<unknown> = new Subject();
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this._getUsers();
+  }
+
+  private _getUsers() {
+    this.userService.getUsers()
+    .pipe(takeUntil(this.endsubs$))
+    .subscribe({
+      next: (users) => {
+        this.users = users;
+        console.log(this.users);
+        setTimeout(() => {
+          $('#table').DataTable({
+            retrieve: true,
+            pagingType: 'full_numbers',
+            pageLength: 5,
+            processing: true,
+            lengthMenu: [5, 10, 25],
+            dom: 'Bfrtip'              
+          });
+        }, 1);
+      },
+      error: (error) => console.error(error)
+    });
   }
 
   onUpdate(id: string) {
-    
+
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  API_URL = environment.apiURL + 'users';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  /**
+   * get all users
+   * @returns 
+   */
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.API_URL);
+  }
+
+  /**
+   * Get details of user by id
+   * @param id 
+   * @returns 
+   */
+  getUser(id:string): Observable<User> {
+    return this.httpClient.get<User>(`${this.API_URL}/${id}`);
+  }
+
+  /**
+   * save new user
+   * @param user 
+   * @returns 
+   */
+  createUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.API_URL, user);
+  }
+
+  /**
+   * update user
+   * @param user 
+   * @returns
+   */
+  updateUser(id: string, user: User): Observable<User> { 
+    return this.httpClient.put<User>(`${this.API_URL}/${id}`, user);
+  }
+  
+  /**
+   * delete user
+   * @param id 
+   * @returns
+   */
+  deleteUser(id: string): Observable<User> {
+    return this.httpClient.delete<User>(`${this.API_URL}/${id}`);
+  }
 }
