65c819b7b79b6dbf25fa1e98968fe23ba30c7c12
Jose <josebratt@gmail.com>
2022-12-31 10:25:46 -0400

    update product details route

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: FeaturesComponent },
       { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailsComponent },
     ]
   },
 ];
