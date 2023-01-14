17bb8ba583050f347e87e96c56b8238da06f6764
Jose <josebratt@gmail.com>
2023-01-14 14:58:35 -0400

    update cartpage

--- src/app/home/pages/cart-page/cart-page.component.ts
+++ src/app/home/pages/cart-page/cart-page.component.ts
@@ -1,52 +1,55 @@
 import { CartItemDetailed } from './../../../interfaces/cart';
 import { ProductService } from 'src/app/services/product.service';
 import { CartService } from 'src/app/services/cart.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css']
+  styleUrls: ['./cart-page.component.css'],
 })
-export class CartPageComponent implements OnInit {
-
+export class CartPageComponent implements OnInit, OnDestroy {
   productDatails: CartItemDetailed[] = [];
   quantity = 0;
+
+  endSubs$: Subject<unknown> = new Subject();
 
   constructor(
     private cartService: CartService,
     private productService: ProductService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this._getCartDetails();
   }
 
-  private _getCartDetails() {
-    this.cartService.cart$.pipe().subscribe(
-      (cart) => { 
-        cart.items!.forEach(item => {
-          this.productService.getProduct(item.productId!).subscribe(
-            (data) => {
-              this.productDatails.push({
-                product: data,
-                quantity: item.quantity
-              });
-            }
-          )
-        })
-      }
-    )
+  ngOnDestroy(): void {
+    this.endSubs$.next(true);
+    this.endSubs$.complete();
   }
 
-  onRemove() {
+  private _getCartDetails() {
+    this.cartService.cart$.pipe(takeUntil(this.endSubs$)).subscribe((cart) => {
+      this.productDatails = [];
+      cart.items!.forEach((item) => {
+        this.productService.getProduct(item.productId!).subscribe((data) => {
+          this.productDatails.push({
+            product: data,
+            quantity: item.quantity,
+          });
+        });
+      });
+    });
+  }
 
+  onRemove(cartItem: CartItemDetailed) {
+    this.cartService.delCartItem(cartItem.product.id);
   }
 
   onBacktoShop() {
     this.router.navigateByUrl('/products');
   }
-
 }
