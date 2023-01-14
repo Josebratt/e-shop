d92096d481328875b8f4e61f9acd87e720f741f6
Jose <josebratt@gmail.com>
2023-01-14 15:41:08 -0400

    update orderservice

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  API_URL_ORDERS = environment.apiURL + 'orders';
+  API_URL_PRODUCTS = environment.apiURL + 'products';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getProduct(productId: string): Observable<any> {
+    return this.httpClient.get<any>(`${this.API_URL_PRODUCTS}/${productId}`);
+  }
 }
