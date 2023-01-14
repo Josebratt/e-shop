f079561cf589d916950dc5b11296a298fa6ac323
Jose <josebratt@gmail.com>
2023-01-14 14:16:11 -0400

    cart-page pdate, create order summary

--- src/app/home/pages/cart-page/cart-page.component.ts
+++ src/app/home/pages/cart-page/cart-page.component.ts
@@ -1,4 +1,8 @@
+import { CartItemDetailed } from './../../../interfaces/cart';
+import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
-  constructor() { }
+  productDatails: CartItemDetailed[] = [];
+  quantity = 0;
+
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this._getCartDetails();
+  }
+
+  private _getCartDetails() {
+    this.cartService.cart$.pipe().subscribe(
+      (cart) => { 
+        cart.items!.forEach(item => {
+          this.productService.getProduct(item.productId!).subscribe(
+            (data) => {
+              this.productDatails.push({
+                product: data,
+                quantity: item.quantity
+              });
+            }
+          )
+        })
+      }
+    )
+  }
+
+  onRemove() {
+
+  }
+
+  onBacktoShop() {
+    this.router.navigateByUrl('/products');
   }
 
 }
