97d62599b2c89c9a90d66a4561d6ea09030ee0a7
Jose <josebratt@gmail.com>
2023-01-14 10:52:13 -0400

    cart route update

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,7 @@ export const routes: Routes = [
       { path: '', component: FeaturesComponent },
       { path: 'products', component: ProductListComponent },
       { path: 'product/:id', component: ProductDetailsComponent },
+      { path: 'cart', component: CartPageComponent },
     ]
   },
 ];
