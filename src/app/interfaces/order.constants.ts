5b98c92e3130098d50a6ec991b58f6e2dc330c94
Jose <josebratt@gmail.com>
2023-01-22 13:41:23 -0400

    create constants order

---
+++ src/app/interfaces/order.constants.ts
@@ -0,0 +1,17 @@
+export const ORDER_STATUS = {
+    0: {
+        label: 'Pendiente'
+    },
+    1: {
+        label: 'Procesado'
+    },
+    2: {
+        label: 'Enviado'
+    },
+    3: {
+        label: 'Entregado'
+    },
+    4: {
+        label: 'No Entregado'
+    }
+}
