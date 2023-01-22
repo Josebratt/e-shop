8798e5b4193f88ef7691ee9dd61e95f1e02b7e94
Jose <josebratt@gmail.com>
2023-01-22 13:41:56 -0400

    update order model

--- src/app/interfaces/order.ts
+++ src/app/interfaces/order.ts
@@ -3,9 +3,9 @@ import { OrderItem } from './order-item';
 export class Order {
   id?: string;
   orderItems?: OrderItem[];
-  shippingAddress1?: string;
-  shippingAddress2?: string;
+  address?: string;
   city?: string;
+  zip?: string;
   phone?: string;
   status?: number;
   totalPrice?: string;
