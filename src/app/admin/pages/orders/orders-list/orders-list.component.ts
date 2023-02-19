028f7253c2ca3d9e6e14e84c29e1bf2175c43ad1
Jose <josebratt@gmail.com>
2023-02-19 17:23:35 -0400

    orderlist update

--- src/app/admin/pages/orders/orders-list/orders-list.component.ts
+++ src/app/admin/pages/orders/orders-list/orders-list.component.ts
@@ -1,4 +1,8 @@
+import { ORDER_STATUS } from 'src/app/interfaces/order.constants';
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/interfaces/order';
+import { OrderService } from 'src/app/services/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders-list',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersListComponent implements OnInit {
 
-  constructor() { }
+  orders: any[] = [];
+  orderStatus = ORDER_STATUS;
+
+  constructor(
+    private orderService: OrderService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.getOrders().subscribe(data => {
+      this.orders = data; console.log(data);
+    });
+  }
+
+  showOrder(id: string) { 
+    this.router.navigateByUrl(`/admin/order/${id}`);
   }
 
 }
